c4273a4c99426cf89531e347d49af0fd5ca0941b
albrones <albrones1@gmail.com>
2018-01-18 11:55:22 +0100

    Add tests on Enfant and Adulte props

--- src/tp3.spec.js
+++ src/tp3.spec.js
@@ -37,6 +37,9 @@ describe('tp3', function() {
       });
       it('should have niveauScolaire prop', function () {
          var enfant = new Enfant('B','A', 22, 'ZEN');
+         expect(enfant.nom).toBe('B');
+         expect(enfant.prenom).toBe('A');
+         expect(enfant.age).toBe(22);
          expect(enfant.niveauScolaire).toBe('ZEN');
       });
       it('should have toString method with niveau scolaire prop', function () {
@@ -56,6 +59,9 @@ describe('tp3', function() {
       });
       it('should have permis prop', function () {
          var adulte = new Adulte('B', 'A', 22, true);
+         expect(adulte.nom).toBe('B');
+         expect(adulte.prenom).toBe('A');
+         expect(adulte.age).toBe(22);
          expect(adulte.permis).toBe(true);
       });
       it('should have toString method with permis prop', function () {
