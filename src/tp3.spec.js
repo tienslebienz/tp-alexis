e9be5052d1dc38b82f369bcfcc9c6a78115b9b89
tienslebienz <etienne.crombez@zenika.com>
2018-01-19 20:17:26 +0100

    prettify js

--- src/tp3.spec.js
+++ src/tp3.spec.js
@@ -1,95 +1,95 @@
 describe('tp3', function() {
-  var tp3;
-  beforeEach(function() {
-    tp3 = require('./tp3');
-  });
-
-  describe('Personne', function() {
-    var Personne;
+    var tp3;
     beforeEach(function() {
-      Personne = tp3.Personne;
+        tp3 = require('./tp3');
     });
 
-    it('should create object', function() {
-      var personne = new Personne();
-      expect(personne).toBeTruthy();
-    });
-    it('should have properties', function () {
-        var personne = new Personne('B','A', 22);
-        expect(personne.nom).toBe('B');
-        expect(personne.prenom).toBe('A');
-        expect(personne.getAge()).toBe(22);
-    });
-    it('should have toString method showing props', function () {
-        var personne = new Personne('B','A', 22);
-        expect(personne.toString()).toBe("B A 22");
-    });
-  });
+    describe('Personne', function() {
+        var Personne;
+        beforeEach(function() {
+            Personne = tp3.Personne;
+        });
 
-  describe('Enfant', function () {
-      var Enfant;
-      beforeEach(function () {
-          Enfant = tp3.Enfant;
-      });
-      it('should create object', function() {
-          var enfant = new Enfant();
-          expect(enfant).toBeTruthy();
-      });
-      it('should have niveauScolaire prop', function () {
-         var enfant = new Enfant('B','A', 22, 'ZEN');
-         expect(enfant.nom).toBe('B');
-         expect(enfant.prenom).toBe('A');
-         expect(enfant.getAge()).toBe(22);
-         expect(enfant.niveauScolaire).toBe('ZEN');
-      });
-      it('should have toString method with niveau scolaire prop', function () {
-          var enfant = new Enfant('B','A', 22, 'ZEN');
-          enfant.niveauScolaire = "zenika";
-          expect(enfant.toString()).not.toBe("B A 22 ZEN");
-          expect(enfant.toString()).toBe("B A 22 zenika");
-      });
-  });
+        it('should create object', function() {
+            var personne = new Personne();
+            expect(personne).toBeTruthy();
+        });
+        it('should have properties', function() {
+            var personne = new Personne('B', 'A', 22);
+            expect(personne.nom).toBe('B');
+            expect(personne.prenom).toBe('A');
+            expect(personne.getAge()).toBe(22);
+        });
+        it('should have toString method showing props', function() {
+            var personne = new Personne('B', 'A', 22);
+            expect(personne.toString()).toBe('B A 22');
+        });
+    });
 
-  describe('Adulte', function () {
-      var Adulte;
-      beforeEach(function () {
-          Adulte = tp3.Adulte;
-      });
-      it('should create object', function() {
-          var adulte = new Adulte();
-          expect(adulte).toBeTruthy();
-      });
-      it('should have permis prop', function () {
-         var adulte = new Adulte('B', 'A', 22, true);
-         expect(adulte.nom).toBe('B');
-         expect(adulte.prenom).toBe('A');
-         expect(adulte.getAge()).toBe(22);
-         expect(adulte.permis).toBe(true);
-      });
-      it('should have toString method with permis prop', function () {
-          var adulte = new Adulte('B','A', 22, true);
-          adulte.permis = false;
-          expect(adulte.toString()).toBe("B A 22 false")
-          expect(adulte.toString()).not.toBe("B A 22 true")
-      });
-  });
+    describe('Enfant', function() {
+        var Enfant;
+        beforeEach(function() {
+            Enfant = tp3.Enfant;
+        });
+        it('should create object', function() {
+            var enfant = new Enfant();
+            expect(enfant).toBeTruthy();
+        });
+        it('should have niveauScolaire prop', function() {
+            var enfant = new Enfant('B', 'A', 22, 'ZEN');
+            expect(enfant.nom).toBe('B');
+            expect(enfant.prenom).toBe('A');
+            expect(enfant.getAge()).toBe(22);
+            expect(enfant.niveauScolaire).toBe('ZEN');
+        });
+        it('should have toString method with niveau scolaire prop', function() {
+            var enfant = new Enfant('B', 'A', 22, 'ZEN');
+            enfant.niveauScolaire = 'zenika';
+            expect(enfant.toString()).not.toBe('B A 22 ZEN');
+            expect(enfant.toString()).toBe('B A 22 zenika');
+        });
+    });
 
-  describe('Personne - modify', function () {
-      var Personne;
-      beforeEach(function() {
-          Personne = tp3.Personne;
-      });
-      it('shouldnt modify age directly', function () {
-          var personne = new Personne('B', 'A', 22);
-          personne.age = 23;
-          expect(personne.getAge()).not.toBe(23);
-          expect(personne.getAge()).toBe(22);
-      });
-      it('shouldnt modify name', function () {
-          var personne = new Personne('B', 'A', 22);
-          personne.nom = 'toto';
-          expect(personne.nom).not.toBe('toto');
-          expect(personne.nom).toBe('B');
-      });
-  });
+    describe('Adulte', function() {
+        var Adulte;
+        beforeEach(function() {
+            Adulte = tp3.Adulte;
+        });
+        it('should create object', function() {
+            var adulte = new Adulte();
+            expect(adulte).toBeTruthy();
+        });
+        it('should have permis prop', function() {
+            var adulte = new Adulte('B', 'A', 22, true);
+            expect(adulte.nom).toBe('B');
+            expect(adulte.prenom).toBe('A');
+            expect(adulte.getAge()).toBe(22);
+            expect(adulte.permis).toBe(true);
+        });
+        it('should have toString method with permis prop', function() {
+            var adulte = new Adulte('B', 'A', 22, true);
+            adulte.permis = false;
+            expect(adulte.toString()).toBe('B A 22 false');
+            expect(adulte.toString()).not.toBe('B A 22 true');
+        });
+    });
+
+    describe('Personne - modify', function() {
+        var Personne;
+        beforeEach(function() {
+            Personne = tp3.Personne;
+        });
+        it('shouldnt modify age directly', function() {
+            var personne = new Personne('B', 'A', 22);
+            personne.age = 23;
+            expect(personne.getAge()).not.toBe(23);
+            expect(personne.getAge()).toBe(22);
+        });
+        it('shouldnt modify name', function() {
+            var personne = new Personne('B', 'A', 22);
+            personne.nom = 'toto';
+            expect(personne.nom).not.toBe('toto');
+            expect(personne.nom).toBe('B');
+        });
+    });
 });
